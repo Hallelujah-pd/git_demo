bd8b9ff68f8bc05859c0173d2b1df5dedceb7842
code_mans <1340420664@qq.com>
2022-09-16 22:06:29 +0800

    Initial commit

---
+++ src/java/Test.java
@@ -0,0 +1,14 @@
+package java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author pd
+ * DateTime: 2022/9/16 21:26
+ */
+public class Test {
+  void test (){
+    System.out.println("holle");
+  }
+}
